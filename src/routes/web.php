cdb7cd650da5014571a3a2805145d50b99ba7b01
髙田 康太 <takadakota@LAb-N1635.local>
2019-12-02 18:42:53 +0900

    コミットメッセージ

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,7 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
-//Route::get('/user', 'UserController@index');
+Route::get('/user', 'UserController@index');
 Route::get('/takadakota', 'TakadaController@index');
 Route::get('/bbs', 'BbsController@index');
 Route::post('/bbs', 'BbsController@create');
@@ -22,9 +22,6 @@ Route::get('github', 'Github\GithubController@top');
 Route::post('github/issue', 'Github\GithubController@createIssue');
 Route::get('login/github', 'Auth\LoginController@redirectToProvider');
 Route::get('login/github/callback', 'Auth\LoginController@handleProviderCallback');
-//Route::post('user', 'User\UserController@updateUser');
+Route::post('user', 'User\UserController@updateUser');
 Route::get('/', 'HomeController@index');
 Route::post('/upload', 'HomeController@upload');
-
-Route::get('/', 'InstaController@index');
-Route::post('/upload', 'InstaController@upload');
